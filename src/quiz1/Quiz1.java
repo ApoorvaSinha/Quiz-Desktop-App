1061cdb7304ab9d6fb397efddfbcc31335d1d1ca
ApoorvaSinha <apoorvasinha701@gmail.com>
2020-08-15 16:27:27 +0530

    Update README.md

---
+++ src/quiz1/Quiz1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz1;
+
+/**
+ *
+ * @author admin
+ */
+public class Quiz1 {
+static Login1 obj;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        obj=new Login1();
+        obj.setVisible(true);
+    }
+    
+}
